df7993f581417cce1b14648970e6801ffc3d85f9
ksheerasagar09 <47727203+ksheerasagar09@users.noreply.github.com>
2019-12-06 12:24:41 +0530

    Add files via upload

---
+++ alpha_api_project.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import os
+import time as t
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+import requests
+import alphavantage
+from alpha_vantage.alphavantage import AlphaVantage
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+from alpha_vantage.sectorperformance import SectorPerformances
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+from alpha_vantage.foreignexchange import ForeignExchange
+from flask import Flask, Response, stream_with_context
+api_key = os.environ['API_KEY']
+
+
+# In[36]:
+
+
+from alpha_vantage.timeseries import TimeSeries
+ts = TimeSeries(key=api_key)
+data, meta_data = ts.get_intraday('GOOGL')
+
+
+# In[31]:
+
+
+meta_data
+
+
+# In[34]:
+
+
+ts = TimeSeries(key=api_key)
+data, meta_data = ts.get_intraday('GOOGL')
+
+
+# In[33]:
+
+
+meta_data
+
+
+# In[35]:
+
+
+data
+
+
+# In[6]:
+
+
+cur = conn.cursor()
+cur.execute("""
+   CREATE TABLE IF NOT EXISTS Stock_Data_Daily (Symbol varchar(255),
+       period_date date,
+       open_price real,
+       high_price real,
+       low_price real,
+       close_price real,
+       volume int
+  )
+""")
+cur.execute("""
+   CREATE TABLE IF NOT EXISTS Stock_Data_Weekly (Symbol varchar(255),
+       period_date date,
+       open_price real,
+       high_price real,
+       low_price real,
+       close_price real,
+       volume int
+  )
+""")
+cur.execute("""
+   CREATE TABLE IF NOT EXISTS Stock_Data_Monthly (Symbol varchar(255),
+       period_date date,
+       open_price real,
+       high_price real,
+       low_price real,
+       close_price real,
+       volume int
+  )
+""")
+conn.commit()
+
+
+# In[38]:
+
+
+import time
+while True:
+    ts = TimeSeries(key=api_key)
+    data, meta_data = ts.get_intraday('GOOGL')
+    try:
+        if data["Time Series (Daily)"]:
+            for key,value in data.items():
+                period_date = key
+                open_price = value['1. open']
+                high_price = value['2. high']
+                low_price = value['3. low']
+                close_price = value['4. close']
+                volume = value['5. volume']
+                cur.execute("Insert into Stock_Data_Daily values (%s,%s,%s,%s,%s,%s,%s)",(symbol,period_date,open_price,high_price,low_price,close_price,volume))
+                conn.commit()
+                db_data = cur.fetchone()
+                print(db_data)
+        except KeyError:
+        if data['Note']:
+            print("sleeping for 60 seconds because execeeded number of calls per min ")
+            time.sleep(60)
+
+
+# In[3]:
+
+
+import psycopg2
+conn = psycopg2.connect(dbname = 'ksheerasagar' , user = 'postgres', password = 'postgres')
+cur = conn.cursor()
+# cur.execute("Truncate table Stock_Data")
+cur.execute("select count(*) from Stock_Data")
+conn.commit()
+count2 = cur.fetchone()
+print(count2)
+
